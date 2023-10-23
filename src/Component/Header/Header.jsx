8adea45d31eb11a9502bea14b9c0541d96bbd788
nagaraj6618 <nagaraj516700@gmail.com>
2023-10-23 20:17:25 +0530

    Bug fixed

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Link } from 'react-router-dom'
+import {BrowserRouter, Link, Routes,Route} from 'react-router-dom'
+import Home from '../../Pages/Home'
+import Login from '../../Pages/Login/Login'
 const Header = () => {
    return (
       <BrowserRouter>
          <Link to='/home'>Home</Link>
          <Link to='/login'>Login</Link>
+         <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/home' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+         </Routes>
       </BrowserRouter>
    )
 }
--- src/Component/Layout/Layout.jsx
+++ src/Component/Layout/Layout.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import Header from '../Header/Header'
-// import Footer from '../Footer/Footer'
-import Router from '../../Router/Router'
-
 const Layout = () => {
    return (
       <React.Fragment>
          <Header />
          {/* <Footer /> */}
-         <Router />
+      
       </React.Fragment >
    )
 }
